00db35c06f3c992760c08964ef9d54d859ac4d34
0oPhantom0o <mahdinoori@outlook.com>
2024-03-14 04:51:40 +0330

    first commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+/*
+	Given an array of integers your solution should find the smallest integer.
+
+For example:
+
+	Given [34, 15, 88, 2] your solution will return 2
+	Given [34, -345, -1, 100] your solution will return -345
+
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
+*/
+
+func main() {
+	smallestNumber()
+}
+
+func smallestNumber() {
+	number := []int{34, 15, 88, 2, 34, -345, -1, 100}
+	emar := []int{1}
+	emar[0] = number[0]
+
+	for _, a := range number {
+		if a <= emar[0] {
+			emar[0] = a
+			fmt.Println(emar[0])
+		}
+
+	}
+	fmt.Println(emar[0])
+	//return emar[0]
+}
